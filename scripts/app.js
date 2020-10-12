064cc8bcbbfb5a5c95555d5052ab748e03d4b58b
ryan woods <federer5050@gmail.com>
2020-10-12 17:35:20 +0100

    destructuring and adding UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,38 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    // destructure properties 
+    const { cityDets, weather } = data;
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+}
+
+
 
 const updateCity = async (city) => {
 
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
 
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+    return { cityDets, weather };
 
-}
+    };
 
 cityForm.addEventListener('submit', e => {
     // prevent default action - doesn't refresh page
@@ -23,7 +45,7 @@ cityForm.addEventListener('submit', e => {
 
     // upadte the UI with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err))
 
 });
